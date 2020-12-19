096bf3993377b8e6cecfe4a80f007fa00141b455
egeardic <52965410+egeardic@users.noreply.github.com>
2020-12-20 00:17:41 +0300

    Add files via upload

---
+++ OCR2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace OCR2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Scanning";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(332, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(244, 88);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1384, 479);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(611, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select Area";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(789, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 67);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Confirm Area";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dimensions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ImgToText";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OCR2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Tesseract;
+
+namespace OCR2
+{
+    public partial class Form1 : Form
+    {
+        public int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ScreenShot();
+        }
+        public void OCR()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var img = new Bitmap(openFileDialog1.FileName);
+                var ocr = new TesseractEngine("./tessdata", "eng");
+                var sonuc = ocr.Process(img);
+                richTextBox1.Text = sonuc.GetText();
+            }
+        }
+        public void ScreenShot()
+        {
+            this.Hide(); //Hide form
+            System.Threading.Thread.Sleep(1000); //Hiding time (Enough to get a screenshot)
+            SendKeys.Send("{PRTSC}");
+            Image myImage = Clipboard.GetImage();
+            pictureBox1.Image = myImage;
+            myImage.Save(Directory.GetCurrentDirectory() +"//Images//" + i.ToString() +"screenshot.jpg");
+            this.Show();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ScreenShot();
+            pictureBox1.Image = new Bitmap(Directory.GetCurrentDirectory() + "//Images//" + i.ToString() + "screenshot.jpg");
+            i++;
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            pictureBox1.MouseEnter += new EventHandler(pictureBox1_MouseEnter);
+            Controls.Add(pictureBox1);
+            
+        }
+        int crpX, crpY, recW, recH;
+        public Pen crpPen = new Pen(Color.White);
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Cursor = Cursors.Cross;
+                crpPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                crpX = e.X;
+                crpY = e.Y;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Dimensions :" + recW + "," + recH;
+            Cursor = Cursors.Default;
+            Bitmap bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(bmp2, pictureBox1.ClientRectangle);
+
+            Bitmap crpImg = new Bitmap(recW, recH);
+
+            for (int i = 0; i < recW; i++)
+            {
+                for (int j = 0; j < recH; j++)
+                {
+                    Color pxlclr = bmp2.GetPixel(crpX + i, crpY + j);
+                    crpImg.SetPixel(i, j, pxlclr);
+                }
+            }
+            pictureBox1.Image = (Image)crpImg;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Cross;
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                pictureBox1.Refresh();
+                recW = e.X - crpX;
+                recH = e.Y - crpY;
+                Graphics g = pictureBox1.CreateGraphics();
+                g.DrawRectangle(crpPen, crpX, crpY, recW, recH);
+                g.Dispose();
+            }
+        }
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Default;
+        }
+
+    }
+}
